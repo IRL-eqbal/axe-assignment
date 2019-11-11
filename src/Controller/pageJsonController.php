f93207e8ae552e67c96a9d4ff1930c626695a282
MD EQBAL AHMAD <mailtomister@gmail.com>
2019-11-11 20:49:12 +0530

    Fixed indentetion: Editor issue

--- src/Controller/pageJsonController.php
+++ src/Controller/pageJsonController.php
@@ -17,14 +17,14 @@ class pageJsonController extends ControllerBase {
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
-	
-	/**
+  
+  /**
    * Constructs a new CalculteCityRoutesdistanceForm object.
    */
   public function __construct(EntityTypeManagerInterface $entity_type_manager) {
     $this->entityTypeManager = $entity_type_manager;
   }
-	
+  
   /**
    * {@inheritdoc}
    */
@@ -33,33 +33,33 @@ class pageJsonController extends ControllerBase {
       $container->get('entity_type.manager')
     );
   }
-	/**
-	 * Calculate and return sortest path between two cities.
+  /**
+   * Calculate and return sortest path between two cities.
    *
-	 * @param string $api_key
-	 *   API Key from system config.
-	 *
-	 * @param int $nid
-	 *   Node ID as route parameters
-	 *
-	 * @return
-	 *  Return node in form of json Object.
-	 *
-	 */
+   * @param string $api_key
+   *   API Key from system config.
+   *
+   * @param int $nid
+   *   Node ID as route parameters
+   *
+   * @return
+   *  Return node in form of json Object.
+   *
+   */
   public function pageJson($api_key, $nid) {
-		$allowed = false;
-		$site_api_key = $this->config('system.site')->get('siteapikey');
-		$node = $this->entityTypeManager->getStorage('node')->load($nid);
-		
-		if ($node->getType() == 'page' && $api_key == $site_api_key) {
-			$allowed = true;
-		}
-		
-		if ($allowed) {
-			return new JsonResponse($node->toArray(), 200, ['Content-Type' => 'application/json']);
-		}
-		else{
-			return new JsonResponse(["error" => "access denied"], 401, ['Content-Type' => 'application/json']);
-		}
-	}
+    $allowed = false;
+    $site_api_key = $this->config('system.site')->get('siteapikey');
+    $node = $this->entityTypeManager->getStorage('node')->load($nid);
+    
+    if ($node->getType() == 'page' && $api_key == $site_api_key) {
+      $allowed = true;
+    }
+    
+    if ($allowed) {
+      return new JsonResponse($node->toArray(), 200, ['Content-Type' => 'application/json']);
+    }
+    else{
+      return new JsonResponse(["error" => "access denied"], 401, ['Content-Type' => 'application/json']);
+    }
+  }
 }
