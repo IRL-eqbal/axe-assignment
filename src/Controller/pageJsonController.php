f133ff4849d1ce75e78083bda5560633689bb08f
MD EQBAL AHMAD <mailtomister@gmail.com>
2019-11-11 20:43:38 +0530

    Removed entity route param; fixed indentetion issue causing by editor

--- src/Controller/pageJsonController.php
+++ src/Controller/pageJsonController.php
@@ -3,37 +3,63 @@
 namespace Drupal\axelerant_assignment\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Access\AccessResult;
-use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Class pageJsonController.
  */
 class pageJsonController extends ControllerBase {
-  
   /**
-   * Returns the loaded node in json response
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
    *
-   * @return string
-   *   Return Hello string.
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  public function pageJson($api_key, NodeInterface $node) {
-    $allowed = false;
-    $site_api_key = $this->config('system.site')->get('siteapikey');
-    
-    if ($node->getType() == 'page' && $api_key == $site_api_key) {
-      $allowed = true;
-    }
-    
-    if ($allowed) {
-      return new JsonResponse($node->toArray(), 200, ['Content-Type' => 'application/json']);
-    }
-    else{
-      return new JsonResponse(["error" => "access denied"], 401, ['Content-Type' => 'application/json']);
-    }
+  protected $entityTypeManager;
+	
+	/**
+   * Constructs a new CalculteCityRoutesdistanceForm object.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
   }
-  
-  
+	
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+	/**
+	 * Calculate and return sortest path between two cities.
+   *
+	 * @param string $api_key
+	 *   API Key from system config.
+	 *
+	 * @param int $nid
+	 *   Node ID as route parameters
+	 *
+	 * @return
+	 *  Return node in form of json Object.
+	 *
+	 */
+  public function pageJson($api_key, $nid) {
+		$allowed = false;
+		$site_api_key = $this->config('system.site')->get('siteapikey');
+		$node = $this->entityTypeManager->getStorage('node')->load($nid);
+		
+		if ($node->getType() == 'page' && $api_key == $site_api_key) {
+			$allowed = true;
+		}
+		
+		if ($allowed) {
+			return new JsonResponse($node->toArray(), 200, ['Content-Type' => 'application/json']);
+		}
+		else{
+			return new JsonResponse(["error" => "access denied"], 401, ['Content-Type' => 'application/json']);
+		}
+	}
 }
